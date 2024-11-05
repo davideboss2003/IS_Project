e0a0b6943a9b312ad6700bcc033621f943cec9a8
davideboss2003 <muresantiberiu55@yahoo.com>
2024-11-05 11:16:41 +0200

    first push

---
+++ movieSite/src/main/java/davide/george/movieSite/MovieSiteApplication.java
@@ -0,0 +1,16 @@
+package davide.george.movieSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MovieSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSiteApplication.class, args);
+	}
+
+
+}
---
+++ movieSite/src/test/java/davide/george/movieSite/MovieSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.george.movieSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
