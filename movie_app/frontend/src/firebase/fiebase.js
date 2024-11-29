193787021741e9c0665a8422e90b5159ea5dfd6c
George Munteanu <george.munteanu.gr@gmail.com>
2024-11-29 15:47:49 +0200

    react front with api

---
+++ movie_app/frontend/src/firebase/fiebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8xzjCDuA8ChzG_VB848c7kw9Kk6kZP_U",
+  authDomain: "movie-site-9e835.firebaseapp.com",
+  projectId: "movie-site-9e835",
+  storageBucket: "movie-site-9e835.firebasestorage.app",
+  messagingSenderId: "310511454643",
+  appId: "1:310511454643:web:600e64c9cccf13460463c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
